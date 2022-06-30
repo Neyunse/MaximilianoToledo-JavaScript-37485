d0189e4a29b62248f437d4080baa509168496922
Neyunse <36079551+Neyunse@users.noreply.github.com>
2022-06-30 18:20:41 -0300

    update files

--- 3-Ciclos-iteraciones/script.js
+++ 3-Ciclos-iteraciones/script.js
@@ -1,1 +1,19 @@
+let i, output,int
+
+const Intput = prompt("Ingresa un numero para multiplicarlo")
+
+int = parseInt(Intput)
+
+if(int !== 0){
+    while(int>0){
+        for(i = 0; i <=100; i++){
+            output = int * i
+        
+            alert(`${int}x${i}=${output}`)
+        }
+        break;
+    }
+}else{
+    alert("No puedes multiplicar 0!")
+}
 
